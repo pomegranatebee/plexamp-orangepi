08c11a045fcbe9ce9d2ee184d3ba22785e266200
Caleb Kirsch <caleb.kirsch@gocollectiv.com>
2024-01-06 16:16:45 -0600

    Updates

--- config.bash
+++ config.bash
@@ -1,7 +1,25 @@
+# After flashing Debian, SSH in as orangepi user
+ssh orangepi@orangepizero2
+
 # Change default password for root and default user
 # Default passwords are "orangepi"
 sudo passwd root
 sudo passwd orangepi
+
+# Update hostname
+sudo nano /etc/hostname # Update hostname from "orangepizero2" to new
+sudo nano /etc/hosts # Update hostname in L2/3 from "orangepizero2" to new
+sudo systemctl restart systemd-hostnamed
+
+# Configure SSH and add public key for remote
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+# After testing SSH with key, remove SSH password access
+sudo nano /etc/ssh/sshd_config # Change "PasswordAuthentication" to no
+sudo systemctl restart sshd
 
 # Update packages
 sudo apt update
@@ -9,10 +27,3 @@ sudo apt upgrade -y
 sudo apt autoremove
 sudo apt clean
 sudo reboot
-
-# SSH in as orangepi user, then configure SSH and add public key for remote
-ssh orangepi@192.168.1.128
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-touch ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
