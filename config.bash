6b8b5c09e2ee3d008e5bf9e810826686f18a1b1c
Caleb Kirsch <caleb.kirsch@gocollectiv.com>
2024-01-06 15:56:45 -0600

    Added package updates and ssh config

--- config.bash
+++ config.bash
@@ -1,10 +1,18 @@
-# User management
+# Change default password for root and default user
+# Default passwords are "orangepi"
 sudo passwd root
-sudo adduser caleb
-sudo usermod -aG sudo caleb
-
+sudo passwd orangepi
 
 # Update packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt update
+sudo apt autoremove
+sudo apt clean
+sudo reboot
+
+# SSH in as orangepi user, then configure SSH and add public key for remote
+ssh orangepi@192.168.1.128
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
