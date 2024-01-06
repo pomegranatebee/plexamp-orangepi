48ee9f4c73c3db4fadaa79c8af522333718adb16
Caleb Kirsch <caleb.kirsch@gocollectiv.com>
2024-01-06 16:26:07 -0600

    Added node.js installation

--- config.bash
+++ config.bash
@@ -27,3 +27,25 @@ sudo apt upgrade -y
 sudo apt autoremove
 sudo apt clean
 sudo reboot
+
+# Install Node.js
+sudo apt-get install -y ca-certificates curl gnupg && sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=16
+echo deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install -y nodejs
+
+
+# Check Node.js version to ensure v16.x.x
+node -v
+
+# If Node.js is not v16.x.x, proceed to install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+sudo reboot
+
+# Confirm that NVM installed successfully
+nvm -v
+
+# Install and select v16.20.2
+nvm install v16.20.2
+nvm use v16.20.2
